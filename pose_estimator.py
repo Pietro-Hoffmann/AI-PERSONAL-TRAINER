f380bad5d8f71e08170e9dc382aadc18f911de72
Pietro-Hoffmann <pietrohoffmann0@gmail.com>
2025-03-30 21:23:52 -0300

    reconhecimento corporal funcionando

--- pose_estimator.py
+++ pose_estimator.py
@@ -5,7 +5,7 @@ import numpy as np # biblioteca para fazer manipulações de arrays (sera utiliz
 from mediapipe import solutions
 from mediapipe.framework.formats import landmark_pb2
 
-file_name='Teste_Supino.mp4'
+file_name='teste03.mp4'
 model_path = 'pose_landmarker_full.task'
 
 
@@ -35,23 +35,40 @@ def draw_landmarks_on_image(rgb_image, detection_result):
 
 
 
-cap = cv2.VideoCapture(file_name)  #Realiza a leitura do arquivo
+
 
 options = python.vision.PoseLandmarkerOptions(
    base_options=python.BaseOptions(model_asset_path=model_path),
    running_mode = python.vision.RunningMode.VIDEO
 )
 
-while (cap.isOpened()): # = enquanto o vídeo estiver aberto retorna True ou False
-    ret, frame = cap.read() # Faz com que toda fez que o cap.read for rodado ele lê um frame diferente do vídeo
-    if ret == True:
-        cv2.imshow('Frame', frame) #vai mostrar essa imagem
+with python.vision.PoseLandmarker.create_from_options(options) as landmarker:
+    calc_ts = [0.0]
+    cap = cv2.VideoCapture(file_name)  #Realiza a leitura do arquivo
 
-        # Press Q on keyboard to exit
-        if cv2.waitKey(25) & 0xFF == ord('q'): # esse comando vem do OpenCV
+    while (cap.isOpened()): # = enquanto o vídeo estiver aberto retorna True ou False
+        ret, frame = cap.read() # Faz com que toda fez que o cap.read for rodado ele lê um frame diferente do vídeo
+        fps = cap.get(cv2.CAP_PROP_FPS) # vai atribuir a fps a quantidade de fps que o video tem
+        # uma lista que vai compondo do tempo com todos os time stamps que foram passando pra cada interação feita no cv2.imshow
+       
+
+        if ret == True:
+            mp_image = mp.Image(image_format=mp.ImageFormat.SRGB, data=frame) # transforma a imagem em compatível com mediapipe
+            calc_ts.append(int(calc_ts[-1] + 1000/fps)) # calculo importante para o mediapipe
+            detection_result = landmarker.detect_for_video(mp_image, calc_ts[-1])
+
+            # a imagem gira com esse código mas a leitura corporal não
+            #frame = cv2.rotate(frame, cv2.ROTATE_90_CLOCKWISE)  # Rotaciona 90° para esquerda
+
+
+            frame = draw_landmarks_on_image(frame, detection_result)
+            cv2.imshow('Frame', frame) #vai mostrar essa imagem
+
+            # Press Q on keyboard to exit
+            if cv2.waitKey(25) & 0xFF == ord('q'): # esse comando vem do OpenCV
+                break
+        else: # caso o ret retornar falso, da um break no while
             break
-    else: # caso o ret retornar falso, da um break no while
-        break
 
 cap.release() # Libera o vídeo
 cv2.destroyAllWindows() # Fecha as janelas que foram abertas
